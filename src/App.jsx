9c65b1914d83f6806f1470b62dcad3cd5035c7d6
SPChandraSai <spchandrasai820@gmail.com>
2024-12-25 03:05:53 +0530

    addrd protected route in main app

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,27 @@ import Login from './Components/Login'
 import Chat from './Components/Chat';
 import Home from './Components/Home';
 import PageNotFound from './Components/PageNotFound';
-import Profile from './Components/Profile';
+import ProtectedRoute from "./Components/ProtectedRoute";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        {/* it will only match with /login string */}
-        <Route path="/login" element={<Login></Login>}></Route>
-        {/* it will match to the route that has chat/sometext */}
-        <Route path="/chat/:uniqueId" element={<Chat></Chat>}></Route>
-        {/* (*) it will match with everything, kind off universal */}
-        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
-      </Routes>
+        <Routes>
+            <Route path="/" element={<ProtectedRoute isLoggedIn={isLoggedIn} >
+              <Home setIsLoggedIn={setIsLoggedIn}></Home>
+            </ProtectedRoute>}></Route>
+            <Route path="/chat/:uniqueId" element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}><Chat></Chat></ProtectedRoute>
+            }></Route>
+            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}></Login>}></Route>
+            <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+        </Routes>
     </>
   )
 }
 
 export default App
+
+
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,26 @@
 import React from 'react'
 import { Fingerprint, LogIn as LoginIcon} from 'lucide-react';
 // another way of using the login icon
-function Login() {
+import { useNavigate } from 'react-router-dom';
+// auth-step-3
+import { signInWithPopup } from 'firebase/auth';
+import {auth} from "../../firebase"
+import { GoogleAuthProvider } from 'firebase/auth';
+
+function Login(props) {
+  const setIsLoggedIn = props.setIsLoggedIn;
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    //login wala logic
+    //auth-step-4
+    const result = await signInWithPopup(auth, new GoogleAuthProvider);
+    console.log(result);
+    setIsLoggedIn(true);
+    // alert("Login");
+    navigate("/");
+  }
   return (
     <>
       <div className='h-[220px] bg-[#04a784] '>
@@ -22,7 +41,7 @@ function Login() {
             <div>
               Sign in with Google
             </div>
-             <LoginIcon />
+             <LoginIcon onClick={handleLogin}/>
           </button>
         </div>
       </div>
