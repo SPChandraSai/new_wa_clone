e40d69e09788431667d76975366543ca4e6c417f
SPChandraSai <spchandrasai820@gmail.com>
2024-12-27 03:54:52 +0530

    placed img into firebase storage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { storage } from '../../firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 function Home() {
+  const handleChanges=(e)=>{
+    console.log("Change Event");
+    const img=e.target.files[0];
+    //address
+    const storageRef=ref(storage, "/profile"+Math.random());
+    //storage task
+    uploadBytesResumable(storageRef, img);
+    uploadTask.on("state_changed", progressCB, errorCB, finishCB);
+    //upload
+    function progressCB(data){
+      console.log("data", data);
+    }
+    //if error
+    function errorCB(error){
+      console.log("error", err);
+    }
+    //on success
+    function finishCB(){
+      console.log("successfully uploaded the file", success);
+      getDownloadURL(uploadTask.snapshot.ref).then(function (url){
+        console.log("url", url);
+      })
+    }
+  }
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      <input type="file"
+      accept='image/png image/jpeg image/webp'
+      onChange={handleChanges}
+      ></input>
+    </>
   )
 }
 
