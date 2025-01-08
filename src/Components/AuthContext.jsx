74c832847afb8626d016ec29f07dbdf3a59b9718
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-01-09 03:20:04 +0530

    worked on cookies

--- src/Components/AuthContext.jsx
+++ src/Components/AuthContext.jsx
@@ -1,16 +1,40 @@
-import { useContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useContext, useEffect, useState } from "react";
+import { auth, db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
 import React from "react";
 
 const AuthContext = React.createContext();
 //hook
-export function userAuth(){
+export function userAuth() {
     return useContext(AuthContext);
 }
 
-function AuthWrapper({children}){
+function AuthWrapper({ children }) {
     const [userData, setUserData] = useState(null);
-
-    return <AuthContext.Provider value={{setUserData, userData}}>
+    useEffect(() => {
+        //checks if u have logged in before
+        //kuch bhi changes honge --> yaha update ho jayega without refreshing the page
+        onAuthStateChanged(auth, async (currentUser) => {
+            if (currentUser) {
+                const docRef = doc(db, "users", currentUser?.uid);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    const { uid, photoURL, displayName, email } = docSnap.data();
+                    //saved the userData into the context
+                    setUserData({
+                        id: uid,
+                        profile_pic: photoURL,
+                        email,
+                        name: displayName
+                    });
+                    console.log("userData Added");
+                }
+            }
+        })
+    }, [])
+    console.log("userData", userData);
+    return <AuthContext.Provider value={{ setUserData, userData }}>
         {children}
     </AuthContext.Provider>
 }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,33 +1,33 @@
 //rfce for the template
 import React from 'react'
-import { Fingerprint, LogIn as LoginIcon} from 'lucide-react';
+import { Fingerprint, LogIn as LoginIcon } from 'lucide-react';
 // another way of using the login icon
 import { useNavigate } from 'react-router-dom';
 // auth-step-3
 import { signInWithPopup } from 'firebase/auth';
-import {auth, db} from "../../firebase"
+import { auth, db } from "../../firebase"
 import { GoogleAuthProvider } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 import { userAuth } from './AuthContext';
 
-async function createUser(authData){
+async function createUser(authData) {
   const userObject = authData.user;
   // const id = userObject.uid;
   // const photoURL = userObject.photoURL;
   // const name = userObject.displayName;
   // const email = userObject.email;
-  const {uid, photoURL, displayName, email}=userObject;
+  const { uid, photoURL, displayName, email } = userObject;
   // console.log("id: ",id," ",photoURL," ",name," ",email);
-  await setDoc(doc(db, "users", uid),{
+  await setDoc(doc(db, "users", uid), {
     email,
-    profile_pic:photoURL,
-    name:displayName
+    profile_pic: photoURL,
+    name: displayName
   })
   // console.log("user data is added");
 }
 
 function Login() {
-  const {setUserData} = userAuth();
+  const { setUserData } = userAuth();
   const navigate = useNavigate();
   const handleLogin = async () => {
     //login wala logic
@@ -36,7 +36,7 @@ function Login() {
     // console.log("result",result);
     await createUser(userData);
     const userObject = userData.user;
-    const {uid, photoURL, displayName, email} = userObject;
+    const { uid, photoURL, displayName, email } = userObject;
     //saved the userData into the context
     setUserData({
       id: uid,
@@ -52,23 +52,23 @@ function Login() {
       <div className='h-[220px] bg-[#04a784] '>
 
         <div className='flex ml-[200px] pt-10 items-center gap-4'>
-          <img src="https://whatsapp-clone-826a9.web.app/whatsapp.svg" alt="" 
-          className='h-8'/>
+          <img src="https://whatsapp-clone-826a9.web.app/whatsapp.svg" alt=""
+            className='h-8' />
           <div className='text-white uppercase font-medium '>Whatsapp</div>
         </div>
       </div>
       <div className='bg-[#eff2f5] h-[calc(100vh-220px)] flex justify-center items-center relative'>
         <div className=' h-[100%] w-[50%] bg-white shadow-2xl flex flex-col gap-4 justify-center items-center absolute top-[-93px]'>
-          <Fingerprint className='h-20 w-20 text-[#04a784]' strokeWidth={1}/>
+          <Fingerprint className='h-20 w-20 text-[#04a784]' strokeWidth={1} />
           <div>Sign In</div>
           <div className='text-gray-500'>Sign in with your google account to get started.</div>
-          <button 
+          <button
             onClick={handleLogin}
-          className='flex gap-2 items-center bg-[#04a784] p-3 text-white rounded-[5px]'>
+            className='flex gap-2 items-center bg-[#04a784] p-3 text-white rounded-[5px]'>
             <div>
               Sign in with Google
             </div>
-             <LoginIcon/>
+            <LoginIcon />
           </button>
         </div>
       </div>
