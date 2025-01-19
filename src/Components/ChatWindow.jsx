7580519dbef162181e8dfb8b9007e614ef561d10
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-01-19 13:17:26 +0530

    message visibility in chat window

--- src/Components/ChatWindow.jsx
+++ src/Components/ChatWindow.jsx
@@ -2,35 +2,38 @@ import { MessageSquareText, PlusIcon, SendIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { db } from '../../firebase';
-import { doc, getDoc, onSnapshot, setDoc } from 'firebase/firestore';
+import { arrayUnion, doc, getDoc, onSnapshot, setDoc, updateDoc } from 'firebase/firestore';
+import { userAuth } from './AuthContext';
 
 function ChatWindow() {
   const params = useParams();
   const [msg, setMsg] = useState("");
   const [secondUser, setSecondUser] = useState();
   const [msgList, setMsgList] = useState([]);
+  const { userData } = userAuth();
 
+  const receiverId = params?.chatId;
   //this is done to generate a unique chat id between two user based on user id.
   const chatId =
     userData?.id > receiverId
       ? `${userData.id}-${receiverId}`
       : `${receiverId}-${userData?.id}`;
 
-  const receiverId = params.chatId
   const handleSendMsg = async () => {
-    if(msg){
+    console.log("msg", msg);
+    if (msg) {
       const date = new Date();
-      const timestamp = date.toLocaleString("en-US",{
+      const timestamp = date.toLocaleString("en-US", {
         hour: "numeric",
-        minutes: "numeric",
+        minute: "numeric",
         hour12: true,
       });
 
       //start chat with the user
-      if(msgList?.length === 0){
-        await setDoc(doc(db, "user-chats", chatId),{
-          chatId:chatId,
-          message: [
+      if (msgList?.length === 0) {
+        await setDoc(doc(db, "user-chats", chatId), {
+          chatId: chatId,
+          messages: [
             {
               text: msg,
               time: timestamp,
@@ -40,16 +43,25 @@ function ChatWindow() {
           ],
         });
       }
-      else{
+      else {
         //update in the message list
-        
+        await updateDoc(doc(db, "user-chats", chatId), {
+          chatId: chatId,
+          //arrayUnion is used here to append last message to the array list.
+          messages: arrayUnion({
+            text: msg,
+            time: timestamp,
+            sender: userData.id,
+            receiver: receiverId,
+          }),
+        });
       }
       setMsg("");
     }
   }
 
   useEffect(() => {
-    //request, data fetch
+    // request , data fetch
     const getUser = async () => {
       const docRef = doc(db, "users", receiverId);
       const docSnap = await getDoc(docRef);
@@ -59,15 +71,17 @@ function ChatWindow() {
       }
     };
     getUser();
+
+    // message list
     const msgUnsubscribe = onSnapshot(doc(db, "user-chats", chatId), (doc) => {
-      setMsgList(doc.data()?.message || []);
+      setMsgList(doc.data()?.messages || []);
     });
 
     return () => {
       msgUnsubscribe();
     }
 
-  }, [receiverId])
+  }, [receiverId]);
 
   //Empty screen
   if (!receiverId)
@@ -101,7 +115,7 @@ function ChatWindow() {
       {/* messages list */}
       <div className="flex-grow flex flex-col gap-12 p-6 ">
         {msgList?.map((m, index) => {
-          <div
+        return <div
             key={index}
             data-sender={m.sender === userData.id}
             className={`bg-white w-fit rounded-md p-2 shadow-sm max-w-[400px] break-words data-[sender=true]:ml-auto data-[sender=true]:bg-primary-light`}
